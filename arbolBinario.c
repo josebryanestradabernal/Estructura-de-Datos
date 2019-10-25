260bb3b3ef2971a0c1bf32cf9826c676dda20b26
Estrada Bernal José Bryan <josbryaneb@gmail.com>
2019-10-24 22:28:06 -0500

    ArbolBinario pre,post,in

---
+++ arbolBinario.c
@@ -0,0 +1,64 @@
+#include "arbolBinario.h"
+#include <stdio.h>
+#include <stdlib.h>
+void  insertData(Arbol *root,int elemento){
+  if(*root==NULL){
+    nodo *node=(nodo*)malloc(sizeof(nodo));
+    *root=node;
+    (*root)->izquierdo=NULL;
+    (*root)->derecho=NULL;
+    (*root)->elemento=elemento;
+  }
+  else{
+    if ((*root)->elemento>=elemento)
+      insertData(&(*root)->izquierdo,elemento);
+    else{
+     insertData(&(*root)->derecho,elemento);}
+  }
+
+}
+void preOrden(Arbol root){
+  if (root==NULL)return;
+  else {
+    preOrden(root->izquierdo);
+    printf("%i ",root->elemento);
+    preOrden(root->derecho);
+
+  }
+}
+void postOrden(Arbol root){
+  if (root==NULL)return;
+  else{
+    postOrden(root->derecho);
+    printf("%i ",root->elemento);
+    postOrden(root->izquierdo);
+  }
+}
+void inOrden(Arbol root){
+  if (root==NULL)return;
+  else{
+    printf("%i ",root->elemento);
+    inOrden(root->izquierdo);
+    inOrden(root->derecho);
+  }
+}
+int main(){
+Arbol root;
+insertData(&root,8);
+insertData(&root,5);
+insertData(&root,10);
+insertData(&root,12);
+insertData(&root,9);
+insertData(&root,2);
+insertData(&root,1);
+insertData(&root,0);
+insertData(&root,17);
+insertData(&root,6);
+preOrden(root);
+printf("\n");
+postOrden(root);
+printf("\n");
+inOrden(root);
+printf("\n");
+
+}
---
+++ arbolBinario.h
@@ -0,0 +1,23 @@
+/*
+Autor: Estrada Bernal Jose Bryan
+fecha: 24 de Oct del 2019
+Este TAD es un Atbol Binario donde se cumple la condicion
+de instertar elementos mas pequeños a la izquierda y elementos mas grandes que la raiz
+a la derecha
+*/
+
+//Estructura del arbol
+#ifndef __ArbolBinario_H
+#define __ArbolBinario_H
+typedef struct nodo{
+  int elemento;
+  struct nodo *derecho;
+  struct nodo *izquierdo;
+}*Arbol, nodo;
+
+//insertar dato
+void insertData(Arbol *root,int elemento);
+void preOrden(Arbol root);
+void postOrden(Arbol root);
+void inOrden(Arbol root);
+#endif
