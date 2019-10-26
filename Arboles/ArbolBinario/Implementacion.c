dfba816c3c390161fac246a6b570fb887bb2f775
josebryanestradabernal <josebryan.amarciano.estradaber@gmail.com>
2019-10-26 17:17:33 -0500

    experimental

---
+++ Arboles/ArbolBinario/Implementacion.c
@@ -0,0 +1,23 @@
+#include "arbolBinario.h"
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+  Arbol root;
+  insertData(&root,8);
+  insertData(&root,5);
+  insertData(&root,10);
+  insertData(&root,12);
+  insertData(&root,9);
+  insertData(&root,2);
+  insertData(&root,1);
+  insertData(&root,0);
+  insertData(&root,17);
+  insertData(&root,6);
+  preOrden(root);
+  printf("\n");
+  postOrden(root);
+  printf("\n");
+  inOrden(root);
+  printf("\n");
+
+  }
--- Arboles/ArbolBinario/arbolBinario.c
+++ Arboles/ArbolBinario/arbolBinario.c
@@ -1,6 +1,4 @@
-#include "arbolBinario.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 void  insertData(Arbol *root,int elemento){
   if(*root==NULL){
     nodo *node=(nodo*)malloc(sizeof(nodo));
@@ -42,23 +40,3 @@ void inOrden(Arbol root){
     inOrden(root->derecho);
   }
 }
-int main(){
-  Arbol root;
-  insertData(&root,8);
-  insertData(&root,5);
-  insertData(&root,10);
-  insertData(&root,12);
-  insertData(&root,9);
-  insertData(&root,2);
-  insertData(&root,1);
-  insertData(&root,0);
-  insertData(&root,17);
-  insertData(&root,6);
-  preOrden(root);
-  printf("\n");
-  postOrden(root);
-  printf("\n");
-  inOrden(root);
-  printf("\n");
-
-  }
