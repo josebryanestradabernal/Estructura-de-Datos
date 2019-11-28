dac764939549190d74273f0edc9e7d04ce5fdaa3
josebryanestradabernal <josebryan.amarciano.estradaber@gmail.com>
2019-11-27 19:45:18 -0600

    Se agrego La circular

---
+++ Listas/CircularSimpleLigada/SimplementeLigada/Implementacion.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include"listaLigadaSimple.h"
+int main (){
+	LISTA list=NULL;
+	agregar(&list,3);
+	agregar(&list,4);
+	agregar(&list,5);
+	printf("%d  \n",mostrar(&list,0));
+	eliminar(&list,0);
+	eliminar(&list,1);
+	printf("%d \n",mostrar(&list,0));
+	agregar(&list,6);
+	agregar(&list,6);
+	agregar(&list,6);
+	agregar(&list,6);
+
+	mostrarLista(list);
+	mostrarLista(list);
+	}
